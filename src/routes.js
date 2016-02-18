f39abf404c6ceb8eff440e976e02a022a39e9b92
Shing Lyu <shing.lyu@gmail.com>
2016-02-18 16:04:27 +0800

    Added result page; rework header and footer

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,14 @@ import { App } from 'containers/App';
 import { Home } from 'containers/Home';
 import { Start } from 'containers/Start';
 import { List } from 'containers/List';
+import { Result } from 'containers/Result';
 
 export default (
   <Route path="/" component={App}>
     <Route path="home" component={Home} />
     <Route path="start" component={Start} />
     <Route path="list" component={List} />
+    <Route path="result" component={Result} />
     <Route status={404} path="*" component={Home} />
   </Route>
 );
