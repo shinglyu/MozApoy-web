a7bb60855fa39ce75f79f0bbebf9e0f00f9a18fa
Shing Lyu <shing.lyu@gmail.com>
2016-02-22 13:17:08 +0800

    Filter by slider

--- js/result.js
+++ js/result.js
@@ -1,24 +1,73 @@
-var Result = React.createClass({
+var mockTestCaseData = [
+  {
+    id: "0001",
+    text: "Login with correct username and password",
+    priority: 1
+  },
+  {
+    id: "0002",
+    text: "Login with invalid username",
+    priority: 2
+  },
+  {
+    id: "0003",
+    text: "Login with invalid password",
+    priority: 3
+  },
+  {
+    id: "0004",
+    text: "Login with password that contains SQL-injection attack",
+    priority: 4
+  },
+];
+
+var Table = React.createClass({
   render: function() {
+    var testcasesElems = this.props.testcases.map(function(testcase){
+      return (
+        <tr>
+          <td> {testcase.id}       </td>
+          <td> {testcase.text}     </td>
+          <td> {testcase.priority} </td>
+        </tr>
+      )
+    });
+
     return (
       <table>
+        <tbody>
         <tr>
-          <th>
-            ID
-          </th>
-          <th>
-            Test Case
-          </th>
+          <th> ID        </th>
+          <th> Test Case </th>
+          <th> Priority</th>
         </tr>
-        <tr>
-          <td>
-            1 
-          </td>
-          <td>
-            Foo
-          </td>
-        </tr>
+        {testcasesElems}
+        </tbody>
       </table>
+    )
+  }
+});
+
+var Result = React.createClass({
+  getInitialState: function(){
+    return {
+      totalTime: 60
+    }
+  },
+
+  filterTestcaseByTotalTime: function(testcases, totalTime){
+    var priority = totalTime / 30; //TODO: improve the algo
+    return testcases.filter(function(tc){return tc.priority <= priority});
+  },
+
+  render: function() {
+    return (
+      <div>
+        <input type="range" min="30" max="120" step="30" defaultValue="60" 
+               onChange={function(evt){this.setState({totalTime: evt.target.value})}.bind(this)}/>
+        <label>Estimated Time: {this.state.totalTime} min</label>
+        <Table testcases={this.filterTestcaseByTotalTime(mockTestCaseData,this.state.totalTime )} />
+      </div>
     )
   }
 });
